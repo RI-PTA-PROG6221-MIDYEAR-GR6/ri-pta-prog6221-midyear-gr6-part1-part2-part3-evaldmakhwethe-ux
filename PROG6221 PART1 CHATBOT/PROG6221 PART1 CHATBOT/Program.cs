4aaf2aad76f8c49b6a74d3edd85bac0e4ab0f566
evaldmakhwethe-ux <evald.makhwethe@gmail.com>
2026-08-05 15:37:49 +0200

    new commit

--- PROG6221 PART1 CHATBOT/PROG6221 PART1 CHATBOT/Program.cs
+++ PROG6221 PART1 CHATBOT/PROG6221 PART1 CHATBOT/Program.cs
@@ -5,7 +5,7 @@ namespace Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!!");
         }
     }
 }
